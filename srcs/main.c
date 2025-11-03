55eeee9504afee6cef3d135f688cc770e3d0da72
Violeta desde 42 <avelandr@cbr8s5.42barcelona.com>
2025-11-03 17:42:39 +0100

    quick changes

--- srcs/main.c
+++ srcs/main.c
@@ -2,20 +2,23 @@
 
 int main(int argc, char **argv, char **env)
 {
-	t_data	data;
+	// t_data	data;
 
 	(void)env;
-	ft_memset(&data, 0, sizeof(t_data));
-	if (!check_execution(&data, argc, argv))
+	// ft_memset(&data, 0, sizeof(t_data));
+	if (!init_shell(argc, argv, env))
 	{
 		print_msg(INPUT_ERROR, RED);
 		return(EXIT_FAILURE);
 	}
-	//	exit_shelly();
+	shell_loop();
+	terminator();
+return (EXIT_SUCCESS);
+}
+
+/*
 	if (data.is_interactive)
 		exect_interactive(&data);
 	else
 		exect_noninteractive(&data, argc, argv);//temporal
-//	exit_shelly(&data);
-	return (EXIT_SUCCESS);
-}
+*/
