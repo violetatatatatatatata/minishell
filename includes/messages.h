7a2460df81a4a819c729a0f84a33dd015d7e2e0f
Violeta desde 42 <avelandr@cbr8s5.42barcelona.com>
2025-11-03 17:18:21 +0100

    arreglar los headers

---
+++ includes/messages.h
@@ -0,0 +1,21 @@
+#ifndef MESSAGES_H
+# define MESSAGES_H
+
+# define ""
+
+// MACROS
+# define	TRUE 1
+# define	FALSE 0
+
+// EXECUTION MESSAGES
+# define PROMPT ">>> shelly: "
+
+// ERRORES
+# define INPUT_ERROR ""
+
+// errores en los builtins
+# define ACCES_MSG			"Search permissions denie for one of the components of path"
+# define ENOENT_MSG			"No such file or directory"
+# define ENAMETOOLONG_MSG	"path is too long"
+
+# endif
--- includes/minishell.h
+++ includes/minishell.h
@@ -9,52 +9,33 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <sys/wait.h>
-# include "libft.h"
 
-// Terminal y entorno
+// terminal y entorno
 # include <termios.h>
 # include <sys/ioctl.h>
 # include <termcap.h>
 
-// Archivos y directorios
+// archivos y directorios
 # include <dirent.h>
 # include <sys/stat.h>
 
-// Senyales
+// senyales
 # include <signal.h>
 
-// Readline
+// readline
 # include <readline/readline.h>
 # include <readline/history.h>
 
+// implemented
 # include "../libft/Includes/libft.h"
 # include "colors.h"
+# include "messages.h"
 
-// Macros
-# define	TRUE 1
-# define	FALSE 0
 
 // deberia ponerlo volatile sigatomic??
 int	g_status;
 
-// definir mensajes de error
-# define INPUT_ERROR "ponermensajeaqui"
-
-// mensajes de ejecucion
-# define PROMPT ">>> shelly: "
-
 // Data structures
-typedef struct	s_data
-{
-	int			is_interactive;
-//	t_token		*token;
-	char		*user_input;
-	char		**env;
-	char		*working_dir;
-	char		*old_working_dir;
-//	t_command	*cmd;
-	pid_t		pid;
-}	t_data;
 
 /*	Definiciones de funciones del proyecto	*/
 
@@ -71,39 +52,6 @@ int	is_redop(const char *str);
 int	is_token(const char *str);
 int	is_word(const char *s);
 
-
 // builtins
-void	enver(char**environ);
-
-// PARSER
-// execution.c
-int		check_execution(t_data *data, int argc, char **argv);
-// parse_input.c
-void	parse_input();
-
-//Pipex
-typedef signed int	t_pid;
-char	*getenvar(char *name, char **env);
-char	*my_getenv(char *name, char **env);
-void	cierra(int in, int out);
-void	finish(int error);
-void	ft_free_pointstring(char **tab);
-void	doblefree(char **a, char **b);
-int		checkacces(char **s_cmd, char **allpath, char **exec);
-int		notempty(char **a, char **b);
-
-// EXECUTE
-void	execute();
-// interactive
-void	reset_prompt(int signo);
-void	exect_interactive(t_data *data);
-// noninteractive
-void	exectscript(t_data *data);
-void	exect_noninteractive(t_data *data, int argc, char **argv);
-void	non_interactive(t_data *data, int argc, char **argv);
-
-// TERMINATOR
-void	free_data();
-// void	exit_shelly();
 
 #endif
