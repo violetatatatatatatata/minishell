4e010f65a06e4435fcfc68b687145507eb73604f
Alex <alexlinux@DESKTOP-HKU38VB.localdomain>
2025-11-22 22:25:35 +0100

    Save changes, fix redirections

--- srcs/execute/ft_exec_cmd_line.c
+++ srcs/execute/ft_exec_cmd_line.c
@@ -44,7 +44,7 @@ static void	ft_child_body(t_values *vals,
 	vals->pids = NULL;
 	close(fd_pipe[0]);
 	dup2(vals->fd_in, STDIN_FILENO);
-	if (vals->fd_prev != vals->fd_in)
+	if (vals->fd_prev > 2 && vals->fd_prev != vals->fd_in)
 		close(vals->fd_prev);
 	fd_out = ft_open_outfile(vals->token);
 	if (fd_out == 1)
@@ -52,7 +52,7 @@ static void	ft_child_body(t_values *vals,
 	else
 		dup2(fd_out, STDOUT_FILENO);
 	close(fd_pipe[1]);
-	if (fd_out > 0)
+	if (fd_out > 2)
 		close(fd_out);
 	return_val = ft_exec_args(vals->args, vals->val_env);
 	ft_free_vals(vals);
@@ -63,10 +63,18 @@ static void	ft_last_cmd(t_values *vals)
 {
 	int	fd_out;
 	int	return_val;
+	int	tmp_in;
 
-	vals->fd_in = ft_open_infile(vals->token);
+	tmp_in = ft_open_infile(vals->token);
 	if (vals->fd_prev == -1)
-		vals->fd_prev = vals->fd_in;
+		vals->fd_in = tmp_in;
+	else
+	{
+		if (tmp_in != STDIN_FILENO)
+			vals->fd_in = tmp_in;
+		else
+			vals->fd_in = vals->fd_prev;
+	}
 	printf("LAST FORK: %i\n", vals->index);
 	printf("Last cmd fd_in: %i\n", vals->fd_in);
 	vals->pids[vals->index] = fork();
@@ -80,7 +88,7 @@ static void	ft_last_cmd(t_values *vals)
 		dup2(vals->fd_in, STDIN_FILENO);
 		if (vals->fd_in > 0)
 			close(vals->fd_in);
-		if (vals->fd_prev >= 0 && vals->fd_prev != vals->fd_in)
+		if (vals->fd_prev > 2 && vals->fd_prev != vals->fd_in)
 			close(vals->fd_prev);
 		fd_out = ft_open_outfile(vals->token);
 		if (fd_out > 2)
@@ -96,12 +104,22 @@ static void	ft_last_cmd(t_values *vals)
 
 static void	ft_command_loop(t_values *vals, int fd_pipe[2])
 {
+	int	tmp_in;
+
+	tmp_in = ft_open_infile(vals->token);
 	printf("FORK: %i\n", vals->index);
 	if (pipe(fd_pipe) == -1)
 		return (perror("Pipe"), free(vals->pids), exit(EXIT_FAILURE));
 	vals->fd_in = ft_open_infile(vals->token);
 	if (vals->fd_prev == -1)
-		vals->fd_prev = vals->fd_in;
+		vals->fd_in = tmp_in;
+	else
+	{
+		if (tmp_in != STDIN_FILENO)
+			vals->fd_in = tmp_in;
+		else
+			vals->fd_in = vals->fd_prev;
+	}
 	vals->pids[vals->index] = fork();
 	if (vals->pids[vals->index] == -1)
 		return (perror("Couldn't create child"),
