98c6cb419cfb7a0500112330b9ba0188c5686025
Violeta desde 42 <avelandr@cbr8s5.42barcelona.com>
2025-12-15 17:18:33 +0100

    pwd arreglado

--- srcs/builtins/pwd.c
+++ srcs/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: avelandr <avelandr@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/23 00:08:07 by avelandr          #+#    #+#             */
-/*   Updated: 2025/11/23 00:08:11 by avelandr         ###   ########.fr       */
+/*   Updated: 2025/12/15 17:17:01 by avelandr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,16 @@ void	bt_pwd(t_shell *data)
 
 	pwd = ft_getenv("PWD", data->env);
 	if (pwd)
-	{
 		ft_putendl_fd(pwd, STDOUT_FILENO);
-		free(pwd);
-	}
 	else
 	{
 		cwd = getcwd(NULL, 0);
 		if (cwd)
 		{
-			ft_putendl_fd(pwd, STDOUT_FILENO);
+			ft_putendl_fd(cwd, STDOUT_FILENO);
 			free(cwd);
 		}
+		else
+			perror("minishell: pwd: error: invalid cwd\n");
 	}
 }
