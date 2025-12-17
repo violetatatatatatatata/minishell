9983e6904490c76258439663734c9b28673d7f3a
Violeta desde 42 <avelandr@cbr8s5.42barcelona.com>
2025-12-17 19:13:28 +0100

    too many lines fixed (separated in two functions)

--- srcs/execute/ft_find_command_path.c
+++ srcs/execute/ft_find_command_path.c
@@ -6,7 +6,7 @@
 /*   By: aalcaide <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 10:55:27 by aalcaide          #+#    #+#             */
-/*   Updated: 2025/12/15 14:39:09 by avelandr         ###   ########.fr       */
+/*   Updated: 2025/12/17 19:12:16 by avelandr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,34 +22,46 @@ static void	ft_free_memory(char **arr)
 	free(arr);
 }
 
-/* getenv == NULL*/
+/*
+** solo busca la ruta válida.
+** retorna la ruta allocada si la encuentra, o NULL si no.
+*/
+static char	*find_correct_path(char **paths, char *cmd_name)
+{
+	char	*correct_path;
+	char	*dir_with_slash;
+	int		i;
+
+	i = 0;
+	while (paths && paths[i])
+	{
+		dir_with_slash = ft_strjoin(paths[i], "/");
+		correct_path = ft_strjoin(dir_with_slash, cmd_name);
+		free(dir_with_slash);
+		if (access(correct_path, F_OK) == 0 && access(correct_path, X_OK) == 0)
+			return (correct_path);
+		free(correct_path);
+		i++;
+	}
+	return (NULL);
+}
+
 char	*ft_find_command_path(char *cmd, t_shell *data)
 {
 	char	**paths;
 	char	**prefixes;
-	char	*ind_path;
-	char	*correct_path;
-	int		i;
+	char	*env_path;
+	char	*final_path;
 
-	i = 0;
-	ind_path = ft_getenv("PATH", data->env);
-	if (ind_path == NULL)
+	env_path = ft_getenv("PATH", data->env);
+	if (env_path == NULL)
 		return (ft_strdup(cmd));
-	paths = ft_split(ind_path, ':');
+	paths = ft_split(env_path, ':');
 	prefixes = ft_split(cmd, ' ');
-	while (paths[i])
-	{
-		ind_path = ft_strjoin(paths[i], "/");
-		correct_path = ft_strjoin(ind_path, prefixes[0]);
-		free(ind_path);
-		if (access(correct_path, F_OK) == 0
-			&& access(correct_path, X_OK) == 0)
-			return (ft_free_memory(paths),
-				ft_free_memory(prefixes), correct_path);
-		free(correct_path);
-		i++;
-	}
+	final_path = find_correct_path(paths, prefixes[0]);
 	ft_free_memory(paths);
 	ft_free_memory(prefixes);
+	if (final_path)
+		return (final_path);
 	return (ft_strdup(cmd));
 }
